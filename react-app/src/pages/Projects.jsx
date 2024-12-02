3b927dcedff7ac7fd04059428d675e8dd5be9454
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-02 12:24:14 +0200

    implementing useprojects in Projects.jsx

--- react-app/src/pages/Projects.jsx
+++ react-app/src/pages/Projects.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Project from "../components/Project";
+import { useProjects } from "../contexts/ProjectsContext";
+
 
 const Projects = () => {
+  const { projects } = useProjects();
   return (
     <div className="projects-container">
       {Projects.map((p) => (
