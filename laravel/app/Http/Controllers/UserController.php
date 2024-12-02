74d1e3b720aee54ee2dbfa563f877080f9eafcb4
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-02 11:44:43 +0200

    creating getUser by id

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
-    //
+class UserController extends Controller {
+    public function getUser($id){
+        $user = Users::find($id);
+        if(!$user){
+            return response()->json([
+                'could not find user with such id'
+            ]);
+        }
+        return response()->json([
+            'successfuly found',
+            $user
+        ]);
+    }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +14,9 @@ Route::get('/{id}',[ProjectController::class,'getProject']);
 Route::post('/project',[ProjectController::class,'setProject']);
 Route::put('/project/{id}',[ProjectController::class,'editProject']);
 Route::delete('/project/{id}',[ProjectController::class,'deleteProject']);
+
+Route::get('/{id}',[UserController::class,'getUser']);
+
+
+
 
