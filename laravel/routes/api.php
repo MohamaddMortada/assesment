930752ba24234274c609b7483c43605b4f5630e4
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-02 11:41:37 +0200

    fixing some bugs in the routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -8,8 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/{id}',ProjectController::class,'getProject');
-Route::post('/project',ProjectController::class,'setProject');
-Route::put('/project/{id}',ProjectController::class,'editProject');
-Route::delete('/project/{id}',ProjectController::class,'deleteProject');
+Route::get('/{id}',[ProjectController::class,'getProject']);
+Route::post('/project',[ProjectController::class,'setProject']);
+Route::put('/project/{id}',[ProjectController::class,'editProject']);
+Route::delete('/project/{id}',[ProjectController::class,'deleteProject']);
 
