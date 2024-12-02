96a507955505712ca0d0be2daba76bc31ec36c12
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-02 12:23:18 +0200

    implementing ProjectContext.jsx

---
+++ react-app/src/context/ProjectsContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+const ProjectsContext = createContext();
+export const ProjectsProvider = ({ children }) => {
+
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const response = await fetch("http://127.0.0.1:8000/api/projects",method);
+            const data = await response.json();
+            setProjects(data);
+          };
+          fetchProjects();
+    },[]);
+
+    return (
+        <ProjectsContext.Provider value={{ projects }}>
+          {children}
+        </ProjectsContext.Provider>
+      );
+}
+export const useProjects = () => {
+  return useContext(ProjectsContext);
+}
